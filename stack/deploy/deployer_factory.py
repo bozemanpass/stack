f4b94c8fb53efa37a84e8a41b162b4df0a0d0549
Thomas E Lackey <telackey@bozemanpass.com>
2025-01-31 15:15:09 -0600

    Add copyright to recently changed files.

--- stack/deploy/deployer_factory.py
+++ stack/deploy/deployer_factory.py
@@ -1,4 +1,5 @@
 # Copyright © 2023 Vulcanize
+# Copyright © 2025 Bozeman Pass, Inc.
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as published by
