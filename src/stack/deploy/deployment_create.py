2e8249750cff95967edbae448096206bff59828e
Thomas E Lackey <telackey@bozemanpass.com>
2025-04-30 21:58:06 -0500

    Fix any-variable-random port mapping to ignore explicit mappings in the compose

--- src/stack/deploy/deployment_create.py
+++ src/stack/deploy/deployment_create.py
@@ -219,12 +219,12 @@ def _get_mapped_ports(stack: Stack, map_recipe: str):
                     for x in range(0, len(ports_array)):
                         orig_port = ports_array[x]
                         # Strip /udp suffix if present
-                        bare_orig_port = orig_port.replace("/udp", "")
+                        bare_orig_port = orig_port.replace("/udp", "").split(":")[-1]
                         # limit to k8s NodePort range
                         random_port = random.randint(30000, 32767)  # Beware: we're relying on luck to not collide
-                        if map_recipe == "any-variable-random":
-                            # This is the default so take no action
-                            pass
+                        if map_recipe in ["any-variable-random", "k8s-clusterip-same"]:
+                            # default for docker and k8s
+                            ports_array[x] = f"{bare_orig_port}"
                         elif map_recipe == "localhost-same":
                             # Replace instances of "- XX" with "- 127.0.0.1:XX"
                             ports_array[x] = f"127.0.0.1:{bare_orig_port}:{orig_port}"
@@ -237,8 +237,6 @@ def _get_mapped_ports(stack: Stack, map_recipe: str):
                         elif map_recipe == "any-fixed-random":
                             # Replace instances of "- XX" with "- 0.0.0.0:<rnd>:XX"
                             ports_array[x] = f"0.0.0.0:{random_port}:{orig_port}"
-                        elif map_recipe == "k8s-clusterip-same":
-                            ports_array[x] = f"{bare_orig_port}"
                         else:
                             print("Error: bad map_recipe")
             else:
