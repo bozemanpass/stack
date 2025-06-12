ca3631e784f81997175b5b0b29a9321bb06e4a72
Thomas E Lackey <telackey@bozemanpass.com>
2025-06-12 17:00:03 -0500

    WIP: Use Containerfile rather than Dockerfile by default.

--- src/stack/data/container-build/bpi-test-database-container/build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Build bpi/test-container
-source ${BPI_CONTAINER_BASE_DIR}/build-base.sh
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-docker build -t bpi/test-database-container:stack -f ${SCRIPT_DIR}/Dockerfile ${build_command_args} $SCRIPT_DIR
