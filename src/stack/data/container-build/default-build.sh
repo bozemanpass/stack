0202daec403f05a831500a64a40d09847ffb1443
Thomas E Lackey <telackey@bozemanpass.com>
2025-06-12 17:13:50 -0500

    Favor Containerfile

--- src/stack/data/container-build/default-build.sh
+++ src/stack/data/container-build/default-build.sh
@@ -10,4 +10,19 @@ if [[ $# -ne 2 ]]; then
 fi
 image_tag=$1
 build_dir=$2
-docker build -t ${image_tag} ${build_command_args} --build-arg BPI_HOST_UID=${BPI_HOST_UID} --build-arg BPI_HOST_GID=${BPI_HOST_GID} ${build_dir}
+
+BPI_CONTAINER_BUILD_CONTAINERFILE=${BPI_CONTAINER_BUILD_CONTAINERFILE}
+if [[ -z ${BPI_CONTAINER_BUILD_CONTAINERFILE} ]]; then
+    if [[ -f ${build_dir}/Containerfile ]]; then
+        BPI_CONTAINER_BUILD_CONTAINERFILE=${build_dir}/Containerfile
+    else
+        BPI_CONTAINER_BUILD_CONTAINERFILE=${build_dir}/Dockerfile
+    fi
+fi
+
+docker build -t ${image_tag} \
+  --file $BPI_CONTAINER_BUILD_CONTAINERFILE \
+  --build-arg BPI_HOST_UID=${BPI_HOST_UID} \
+  --build-arg BPI_HOST_GID=${BPI_HOST_GID} \
+  ${build_command_args} \
+  ${build_dir}
