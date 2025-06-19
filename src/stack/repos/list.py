a93a9a6b38b5b4c9f3640cd355b17b35ba75ee0e
Thomas E Lackey <telackey@bozemanpass.com>
2025-06-19 16:48:39 -0500

    lint

--- src/stack/repos/list.py
+++ src/stack/repos/list.py
@@ -20,26 +20,24 @@
 
 import click
 
-from stack.build.build_util import host_and_path_for_repo
-from stack.config.util import get_config_setting, get_dev_root_path
+from stack.config.util import get_dev_root_path
 from stack.deploy.stack import Stack
 from stack.opts import opts
-from stack.repos.setup_repositories import process_repo, fs_path_for_repo
 from stack.util import error_exit
 
 
-def locate_stacks_beneath(search_path):
+def locate_stacks_beneath(search_path=get_dev_root_path()):
     stacks = []
     if search_path.exists():
-        for path in search_path.rglob('stack.yml'):
+        for path in search_path.rglob("stack.yml"):
             stacks.append(Stack().init_from_file(path))
 
     return stacks
 
 
-def locate_single_stack(search_path, stack_name):
+def locate_single_stack(stack_name, search_path=get_dev_root_path()):
     stacks = locate_stacks_beneath(search_path)
-    candidates = [ s for s in stacks if s.name == stack_name ]
+    candidates = [s for s in stacks if s.name == stack_name]
     if len(candidates) == 1:
         return candidates[0]
     elif len(candidates) > 1:
@@ -49,8 +47,10 @@ def locate_single_stack(search_path, stack_name):
 
 @click.command()
 @click.argument("stack-locator", required=False)
+@click.option("--names-only", is_flag=True, default=False, help="only show stack names")
+@click.option("--paths-only", is_flag=True, default=False, help="only show stack paths")
 @click.pass_context
-def command(ctx, stack_locator):
+def command(ctx, stack_locator, names_only, paths_only):
     """list stacks in a repository"""
     dev_root_path = get_dev_root_path()
     if opts.o.verbose:
@@ -59,10 +59,10 @@ def command(ctx, stack_locator):
     search_path = dev_root_path
 
     stacks = locate_stacks_beneath(search_path)
-    stacks.sort(key=lambda stack: stack.name)
+    stacks.sort(key=lambda s: s.name)
 
     if stack_locator:
-        stacks = [ s for s in stacks if stack_locator in str(s.file_path) or stack_locator in s.name ]
+        stacks = [s for s in stacks if stack_locator in str(s.file_path) or stack_locator in s.name]
 
     max_name_len = 0
     max_path_len = 0
@@ -72,4 +72,9 @@ def command(ctx, stack_locator):
 
     padding = 8
     for stack in stacks:
-        print(f"{stack.name.ljust(max_name_len + padding)} {str(stack.file_path.parent).ljust(max_path_len + 8)} {str(stack.repo_path)[len(str(dev_root_path))+1:]}")
+        if names_only:
+            print(stack.name)
+        elif paths_only:
+            print(str(stack.file_path.parent))
+        else:
+            print(f"{stack.name.ljust(max_name_len + padding)} {str(stack.file_path.parent).ljust(max_path_len + 8)}")
