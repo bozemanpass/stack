9d2199dec3171df57e50c7c4bab626bc75e916cf
David Boreham <david@bozemanpass.com>
2025-09-09 20:01:42 -0600

    Update kind install script

--- tests/scripts/install-kind.sh
+++ tests/scripts/install-kind.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
-# TODO: handle ARM
-curl --silent -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+# For AMD64 / x86_64
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-amd64
+# For ARM64
+[ $(uname -m) = aarch64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.30.0/kind-linux-arm64
 chmod +x ./kind
 mv ./kind /usr/local/bin
