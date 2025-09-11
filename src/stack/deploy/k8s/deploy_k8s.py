8ae4e95785b4f9f687293e9d0aade2fa551fe118
David Boreham <david@bozemanpass.com>
2025-09-11 09:24:43 -0600

    Trigger CI job

--- src/stack/deploy/k8s/deploy_k8s.py
+++ src/stack/deploy/k8s/deploy_k8s.py
@@ -251,7 +251,6 @@ class K8sDeployer(Deployer):
             if ingress:
                 log_debug(f"Sending this ingress: {ingress}")
                 if not opts.o.dry_run:
-                    # Exception thrown here
                     # kubernetes.client.exceptions.ApiException: (500)
                     ingress_resp = self.networking_api.create_namespaced_ingress(namespace=self.k8s_namespace, body=ingress)
                     log_debug("Ingress created:")
